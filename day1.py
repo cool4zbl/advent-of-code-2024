03de13309bed0402aa772b61fc8932c2e43acc45
Binliu Zhang <binliu.zhang@gmail.com>
2024-12-01 18:50:07 +0100

    day1 solved

---
+++ day1.py
@@ -0,0 +1,32 @@
+from typing import List
+import collections
+
+def read_integer_pairs(filepath: str) -> tuple[List[int], List[int]]:
+    lst1, lst2 = [], []
+    with open(filepath, 'r') as file:
+        for line in file:
+            left, right = map(int, line.split())
+            lst1.append(left)
+            lst2.append(right)
+    return lst1, lst2
+
+def distance():
+    lst1, lst2 = read_integer_pairs('./day1.txt')
+
+    lst1.sort()
+    lst2.sort()
+
+    return sum(map(lambda x: abs(x[1] - x[0]), zip(lst1, lst2)))
+
+print(distance())
+
+def similarity():
+    lst1, lst2 = read_integer_pairs('./day1.txt')
+
+    counter1 = collections.Counter(lst1)
+    counter2 = collections.Counter(lst2)
+
+    return sum(counter2[key] * key * freq for key, freq in counter1.items())
+
+print(similarity())
+
