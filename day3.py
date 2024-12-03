81e9f671d8bf04ad53a3b5be69d9c63f31812b80
Binliu <binliu.zhang@gmail.com>
2024-12-03 12:45:43 +0100

    day3 solved; learned py regexp

--- day3.py
+++ day3.py
@@ -1,2 +1,31 @@
-def solution():
-    pass
+import re
+
+pattern1 = r'mul\((\d{1,3}),(\d{1,3})\)'
+pattern = r'mul\((\d{1,3}),(\d{1,3})\)|don\'t\(\)|do\(\)'
+
+def sol():
+    with open('./input/day3.txt', 'r') as file:
+        total = 0
+        for line in file:
+            ms = re.findall(pattern1, line)
+            total += sum([int(x) * int(y) for x, y in ms])
+    return total
+
+print(sol())
+
+def sol2():
+    with open('./input/day3.txt', 'r') as file:
+        total = 0
+        enable = True # it's global scope, not line-scope
+
+        for line in file:
+            for match in re.finditer(pattern, line):
+                if match[0] == 'don\'t()':
+                    enable = False
+                elif match[0] == 'do()':
+                    enable = True
+                elif enable:
+                    total += int(match[1]) * int(match[2])
+    return total
+
+print(sol2())
