372c958e846d9084868a97c550e619659134812d
Binliu <binliu.zhang@gmail.com>
2024-12-16 08:24:27 +0100

    day14 p2 solved

--- day14_2.py
+++ day14_2.py
@@ -1,7 +1,7 @@
 from collections import defaultdict, deque
 import time
 
-# --- Day 12: Garden Groups ---
+# --- Day 14: Restroom Redoubt ---
 
 start_time = time.time()
 
@@ -9,32 +9,43 @@ is_test = False
 day = 14
 input_file = f'./input/day{day}{"_test" if is_test else ""}.txt'
 
-# m, n = 7, 11
-
 def build_grid(m, n):
     return defaultdict(str) | {(i, j): 0 for i in range(m) for j in range(n)}
 
 def sol():
     m, n = 103, 101
 
-    for times in range(2):
-        g = build_grid(m, n)
-        robots = []
+    # calculate maximum product of 4 quadrants
+    robots = []
+    with open(input_file, 'r') as f:
+        for line in f.readlines():
+            p, v = line.split(' ')
+            c, r = p.split('=')[1].split(',')
+            vy, vx = v.split('=')[1].split(',')
+            robots.append((int(r), int(c), int(vy), int(vx)))
 
-        with open(input_file, 'r') as f:
-            for line in f.readlines():
-                p, v = line.split(' ')
-                c, r = p.split('=')[1].split(',')
-                vy, vx = v.split('=')[1].split(',')
-                robots.append((int(r), int(c), int(vy), int(vx)))
+    times = 7055
+    # 5000 < x < 12500
+    while times < 7056:
+        g = build_grid(m, n)
 
         for robot in robots:
             sr, sc, vy, vx = robot
             er, ec = (sr + times * vx) % m, (sc + times * vy) % n
             g[(er, ec)] += 1
 
+        found = True
+        for k, v in g.items():
+            x, y = k
+            if v > 1:
+                found = False
+                break
+
+        if found:
+            print(found, times)
+
         board = [['.' for _ in range(n)] for _ in range(m)]
-        for k,v in g.items():
+        for k, v in g.items():
             x, y = k
             board[x][y] = str(v) if v > 0 else '.'
 
@@ -47,7 +58,8 @@ def sol():
 
         with open(filepath, 'w', encoding='utf-8') as file:
             file.write(output)
-
         print(f'Saved output to {filepath}')
+
+        times += 1
 
 sol()
