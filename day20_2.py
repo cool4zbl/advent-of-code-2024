71d3b5e2cbc27886202bcc3ca07d3f6de4171489
Binliu <binliu.zhang@gmail.com>
2024-12-20 18:23:07 +0100

    day20 p2 wip

---
+++ day20_2.py
@@ -0,0 +1,105 @@
+from collections import defaultdict, deque
+from heapq import heappush, heappop, heapify
+import time
+
+start_time = time.time()
+
+is_test = False
+day = 20
+input_file = f'./input/day{day}{"_test" if is_test else ""}.txt'
+
+# UDLR
+dirs = ((-1, 0), (1, 0), (0, -1), (0, 1))
+acts = ['^', 'v', '<', '>']
+
+def build_grid(ls):
+    return defaultdict(str) | {(i, j): ls[i][j] for i in range(len(ls)) for j in range(len(ls[i]))}, len(ls), len(ls[0])
+
+def sol():
+
+    g, m, n = build_grid(open(input_file, 'r').read().splitlines())
+    sr, sc = next(k for k, v in g.items() if v == 'S')
+    er, ec = next(k for k, v in g.items() if v == 'E')
+    parents = defaultdict(tuple)
+
+    def dijkstra(sr, sc, er, ec):
+        open_list = [(0, sr, sc, 0)]
+        heapify(open_list)
+        closed_set = set()
+        parents[sr, sc] = (sr, sc)
+
+        while open_list:
+            score, r, c, d = heappop(open_list)
+
+            if (r, c) in closed_set:
+                continue
+            closed_set.add((r, c))
+            if (r, c) != (sr, sc):
+                parents[r, c] = (r - dirs[d][0], c - dirs[d][1])
+
+            if (r, c) == (er, ec):
+                return score
+
+            for idx, (dr, dc) in enumerate(dirs):
+                nr, nc = r + dr, c + dc
+                if g[nr, nc] and g[nr, nc] != '#':
+                    new_score = score + 1
+                    heappush(open_list, (new_score, nr, nc, idx))
+        return None
+
+    best = dijkstra(sr, sc, er, ec)
+    print('best', best)
+
+    path = []
+    def construct_path(p, path):
+        r, c = er, ec
+        while (r, c) != (sr, sc):
+            path.append((r, c))
+            r, c = p[r, c]
+        path.append((r, c))
+        return path
+
+    construct_path(parents, path)
+    path.reverse()
+
+    # save-secs, positions
+    cheats = defaultdict(set)
+    for i, (x, y) in enumerate(path):
+        for idx, (dx, dy) in enumerate(dirs):
+            x1, y1 = x + dx, y + dy
+            for j in range(i + 1, len(path)):
+                x2, y2 = path[j]
+
+                if abs(x2 - x1) + abs(y2 - y1) <= 20:
+                    duration = abs(x2 - x1) + abs(y2 - y1)
+                    if g[x1, y1] and g[x1, y1] == '#' and g[x2, y2] and g[x2, y2] != '#':
+                        # pos = path.index((n2x, n2y))
+                        # if pos < i:
+                        #     continue
+                        # fast = pos - i - 2
+                        # TODO: fixme
+                        fast = j - i - (duration + 1)
+                        cheats[fast].add((x, y, x2, y2))
+
+    # 1813535 is wrong.
+    # print('path', path)
+    print('cheats', cheats)
+
+    total = 0
+    for k, v in cheats.items():
+        if k >= 50:
+            print(k, len(v))
+        if k == 76:
+            print(k, v)
+        total += len(v) if k >= 100 else 0
+    print('total', total)
+    # board = [['.' for _ in range(n)] for _ in range(m)]
+    # for k, v in g.items():
+    #     x, y = k
+    #     board[x][y] = v if k not in path else 'O'
+    #
+    # for row in board:
+    #     print(''.join(row))
+
+sol()
+print(f'{(time.time() - start_time) * 1000:.3f}ms')
